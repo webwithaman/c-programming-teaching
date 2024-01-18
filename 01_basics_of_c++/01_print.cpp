7a3c6b513ac8d3ebbdc2b116125bd87c4a5e8e3c
aman <webwithaman06@gmail.com>
2024-01-18 20:04:36 +0530

    add : 01_basics_of_c++ 01

---
+++ 01_basics_of_c++/01_print.cpp
@@ -0,0 +1,19 @@
+// // Write a C++ program to print "cpp-programming" on the screen.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// Add namespace
+using namespace std;
+
+// // Main Function Start
+int main()
+{
+
+    cout << "cpp-programming"; // cout is a predefined object used to print output on screen
+
+    getch();
+    return 0;
+}
+// // Main Function End
