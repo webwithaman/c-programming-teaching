904711439257bd34d78e3f15e0521e16ee57eaaf
aman <webwithaman06@gmail.com>
2024-01-18 20:26:59 +0530

    add : 01_basics_of_c++ 04

---
+++ 01_basics_of_c++/04_area_of_circle.cpp
@@ -0,0 +1,27 @@
+// Write a C++ program to calculate the area of a circle.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// Add namespace
+using namespace std;
+
+// // Main Function Start
+int main()
+{
+    float radius, area;
+
+    cout << "\nEnter Radius of Circle => ";
+    cin >> radius;
+
+    // // calculate area of circle
+    area = 22.0 / 7 * radius * radius;
+
+    cout << "\nArea of Circle Having Radius " << radius << " is " << area;
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
