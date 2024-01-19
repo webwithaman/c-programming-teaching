778a9c3460940e027ddbde549316752fcc1e845e
aman <webwithaman06@gmail.com>
2024-01-19 23:39:24 +0530

    add : 01_functions 05

--- 02_functions/06_swap_using_reference.cpp
+++ 02_functions/06_swap_using_reference.cpp
@@ -1,24 +1,4 @@
-// // Define a function to print Pascal Triangle up to N lines.
-
-/*
-
-              1
-            1   1
-          1   2   1
-        1   3   3   1
-      1   4   6   4   1
-
-      Pascal Traingle
-
-      1st Formula..
-      Find Element For Any Position = row! / ( col! * (row-col)! )
-      where, row =  0 1 2 ...... N and col = 0 1 2 ..... N
-
-      2nd Formula..
-      Find Element For Any Position =>  n =  n * (row - col) / (col + 1).
-      where, n Starts with 1 for Each Row, row =  0 1 2 ...... N and col = 0 1 2 ..... N
-
-*/
+// // Define a function to swap data of two int variables using call by reference.
 
 // // Header files
 #include <iostream>
@@ -71,15 +51,4 @@ void pascalTriangle(int maxRows)
 
         cout << endl;
     }
-}
-
-// // Function to Find Factorial
-long long unsigned int factorial(int num)
-{
-    long long unsigned fact = 1;
-
-    for (int i = 2; i <= num; i++)
-        fact *= i;
-
-    return fact;
 }
