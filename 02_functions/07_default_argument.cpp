ed0f18a7568fcb70dd5e14bcaf1e0c60a7e690de
aman <webwithaman06@gmail.com>
2024-01-19 23:47:25 +0530

    add : 01_functions 07

---
+++ 02_functions/07_default_argument.cpp
@@ -0,0 +1,42 @@
+// // Write a function using the default argument that is able to add 2 or 3 numbers.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#include <iomanip>
+
+// // Add namespace
+using namespace std;
+
+// // Function Declaration (Prototype)
+int add(int, int, int = 0); // default argument
+
+// // Main Function Start
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "\nEnter 2 Numbers to Add => ";
+    cin >> num1 >> num2;
+
+    cout << "\nSum of " << num1 << " and " << num2 << " => " << add(num1, num2);
+
+    cout << "\n\nEnter 3 Numbers to Add => ";
+    cin >> num1 >> num2 >> num3;
+
+    cout << "\nSum of " << num1 << ", " << num2 << " and " << num3 << " => " << add(num1, num2, num3);
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function Definition
+
+// // Function to Print Pascal Triangle Upto N Lines
+int add(int num1, int num2, int num3)
+{
+    return num1 + num2 + num3;
+}
