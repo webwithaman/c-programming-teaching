873cee1f3dc3986de911ae963fb1ae214460f72b
aman <webwithaman06@gmail.com>
2024-01-19 23:56:09 +0530

    add : 01_functions 08

--- 02_functions/08_overloaded_area_fun.cpp
+++ 02_functions/08_overloaded_area_fun.cpp
@@ -1,1 +1,48 @@
-// // Define overloaded functions to calculate area of circle, area of rectangle and area of triangle.
+// // Define overloaded functions to calculate area of circle, area of rectangle.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+// // Add namespace
+using namespace std;
+
+// // Function Declaration (Prototype)
+float area(float);
+float area(float, float);
+
+// // Main Function Start
+int main()
+{
+    float l, b, radius;
+
+    cout << "\nEnter Length and Breadth of Rectangle to Find Area => ";
+    cin >> l >> b;
+
+    cout << "\nArea of Rectangle => " << area(l, b);
+
+    cout << "\n\nEnter Radius of Circle to Find Area => ";
+    cin >> radius;
+
+    cout << "\nArea of Circle => " << area(radius);
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function Definition
+
+// // Overloaded Function to Find Area of Rectangle
+float area(float l, float b)
+{
+    return l * b;
+}
+
+// // Overloaded Function to Find Area of Circle
+float area(float radius)
+{
+    return 22.0 / 7 * radius * radius;
+}
