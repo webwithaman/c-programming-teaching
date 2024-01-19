9cf0064f742fd4f7d27462eb962e14ff0820bcea
aman <webwithaman06@gmail.com>
2024-01-19 23:00:40 +0530

    add : 01_functions 03

---
+++ 02_functions/04_is_term_in_fibonacci.cpp
@@ -0,0 +1,56 @@
+// // Define a function to check whether a given number is a term in a Fibonacci  series or not.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+// // Add namespace
+using namespace std;
+
+// // Function Declaration (Prototype)
+int isTermInFibonacci(int);
+
+// // Main Function Start
+int main()
+{
+
+    int num;
+
+    cout << "\nEnter A Number to Check Whether It is a Term In Fibonacci Series or Not => ";
+    cin >> num;
+
+    if (isTermInFibonacci(num))
+    {
+        cout << "\nYes, " << num << " is a Term in Fibonacci Series";
+    }
+    else
+    {
+        cout << "\nNo, " << num << " is not a Term in Fibonacci Series";
+    }
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function Definition
+
+// // Function to Check whether A Given Number is a Term in Fibonacci Series or Not
+int isTermInFibonacci(int num)
+{
+    int prev = -1, next = 1, res = 0;
+
+    while (res < num)
+    {
+        res = prev + next;
+        prev = next;
+        next = res;
+    }
+
+    if (res == num)
+        return 1; // Yes, num is a term in fibonacci series
+
+    return 0; // No, num is not a term in fibonacci series
+}
