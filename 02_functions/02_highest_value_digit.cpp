4f647a9fdf2ac746b6be12243661a5470c07e6d0
aman <webwithaman06@gmail.com>
2024-01-19 22:34:09 +0530

    add : 01_functions 02

---
+++ 02_functions/02_highest_value_digit.cpp
@@ -0,0 +1,50 @@
+// // Define a function to find the highest value digit in a given number.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+// // Add namespace
+using namespace std;
+
+// // Function Declaration (Prototype)
+int highestDigit(int);
+
+// // Main Function Start
+int main()
+{
+
+    int num, highDigit;
+    cout << "\nEnter A Number => ";
+    cin >> num;
+
+    highDigit = highestDigit(num);
+
+    cout << "\nHighest value Digit of " << num << " => " << highDigit;
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function Definition
+
+// // Function to Check Whether A Given Number is Prime or Not
+int highestDigit(int num)
+{
+    int highDigit = num % 10;
+
+    num /= 10;
+
+    while (num)
+    {
+        if (num % 10 > highDigit)
+            highDigit = num % 10;
+
+        num /= 10;
+    }
+
+    return highDigit;
+}
