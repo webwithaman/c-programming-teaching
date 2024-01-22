4e96ac08f4e62b92fe76937c62b6eeca0286189e
aman <webwithaman06@gmail.com>
2024-01-22 20:23:19 +0530

    add : classes_and_objects 01

--- classes_and_objects/01_complex.cpp
+++ classes_and_objects/01_complex.cpp
@@ -5,9 +5,24 @@
 #include <conio.h>
 #include <math.h>
 
+// // define class Complex
+class Complex
+{
+    float real;
+    float imaginary;
+
+public:
+    void setComplex()
+    {
+    }
+
+    void printComplex()
+    {
+    }
+};
+
 // // Main Function Start
 int main()
 {
-    
 }
 // // Main Function End
