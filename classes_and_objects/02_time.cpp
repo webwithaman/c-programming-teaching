90f67811191226e7bbef8abd39339415b89ad3bb
aman <webwithaman06@gmail.com>
2024-01-22 20:43:30 +0530

    add : classes_and_objects 02

---
+++ classes_and_objects/02_time.cpp
@@ -0,0 +1,53 @@
+// // Define a class Time to represent Time (like 3 hr 45 min 20 sec). Declare appropriate number of instance member variables and also define instance member functions to set values for time and display values of time.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Complex
+class Complex
+{
+    float real;
+    float imag;
+
+public:
+    void setComplex(float r, float i)
+    {
+        real = r;
+        imag = i;
+    }
+
+    void printComplex()
+    {
+        cout << "\n"
+             << real << " + " << imag << "i" << endl;
+    }
+};
+
+// // Main Function Start
+int main()
+{
+    Complex c1; // create object of Complex
+    float real, imag;
+
+    // Get Complex number
+    cout << "\n>>>>>>>> Enter A Complex Number <<<<<<<<<\n";
+    cout << "\nEnter Real Part => ";
+    cin >> real;
+    cout << "\nEnter Imaginary Part => ";
+    cin >> imag;
+
+    c1.setComplex(real, imag); // set complex number
+
+    cout << "\n>>>>>>>> Entered Complex Number <<<<<<<<<";
+    c1.printComplex(); // print complex number
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
