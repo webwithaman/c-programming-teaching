b84b4cf59103c8655059ba82c9af694e09fddc23
aman <webwithaman06@gmail.com>
2024-01-22 20:18:31 +0530

    add : classes_and_objects 01

--- classes_and_objects/01_complex.cpp
+++ classes_and_objects/01_complex.cpp
@@ -1,4 +1,4 @@
-// // Define a function to check whether a given number is a Prime number or not.
+// // Define a class Complex to represent a complex number. Declare instance member variables to store real and imaginary part of a complex number, also define instance member functions to set values of complex number and print values of complex number.
 
 // // Header files
 #include <iostream>
