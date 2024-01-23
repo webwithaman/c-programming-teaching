2cd2061e8e8dd8a490e45ab010bd6bf51f7ad7bf
aman <webwithaman06@gmail.com>
2024-01-23 16:32:32 +0530

    add : cpp_programming_questions 04

--- 03_classes_and_objects/04_largest_of_two.cpp
+++ 03_classes_and_objects/04_largest_of_two.cpp
@@ -7,58 +7,84 @@
 // // use namespace
 using namespace std;
 
-// // define class LargestOfTwo
-class LargestOfTwo
+// // define class LargestNumber
+class LargestNumber
 {
-    // instance member variable
-    int num;
-    long long unsigned int factorial;
+    // instance member variables
+    int num1, num2, num3, largest;
 
 public:
-    // instance member function to set number for which the factorial will be calculated
-    void setNumber(int n)
+    // instance member function to set number 2 numbers
+    void setNumbers(int n1, int n2)
     {
-        num = n;
+        num1 = n1;
+        num2 = n2;
     }
 
-    // instance member function to display number for which the factorial will be calculated
-    void displayNumber()
+    // instance member function to set number 3 numbers
+    void setNumbers(int n1, int n2, int n3)
     {
-        cout << num;
+        num1 = n1;
+        num2 = n2;
+        num3 = n3;
     }
 
-    // instance member function to calculate factorial of num
-    void calculateLargestOfTwo()
+    // instance member function to find largest of 2 numbers
+    void findLargestOfTwo()
     {
-        factorial = 1;
-        for (int i = 2; i <= num; i++)
-            factorial *= i;
+        largest = num1 > num2 ? num1 : num2;
     }
 
-    // instance member function to display the factorial
-    void displayLargestOfTwo()
+    // instance member function to find largest among three  numbers
+    void findLargestOfThree()
     {
-        cout << factorial;
+        if (num1 > num2)
+        {
+            if (num1 > num3)
+                largest = num1;
+            else
+                largest = num3;
+        }
+        else if (num2 > num3)
+            largest = num2;
+        else
+            largest = num3;
+    }
+
+    // instance member function to display the largest number
+    void displayLargestNumber()
+    {
+        cout << largest;
     }
 };
 
 // // Main Function Start
 int main()
 {
-    LargestOfTwo f1; // create object of LargestOfTwo
-    int n;
+    LargestNumber l1; // create object of LargestNumber
+    int n1, n2, n3;
 
-    // Get a number to calculate factorial
-    cout << "\nEnter A Number to Calculate LargestOfTwo => ";
-    cin >> n;
+    // Get two numbers to find largest of 2 numbers
+    cout << "\nEnter Two Numbers to Find Largest => ";
+    cin >> n1 >> n2;
 
-    f1.setNumber(n); // set Number to calculate factorial
+    l1.setNumbers(n1, n2); // set numbers to find largest of two numbers
+    l1.findLargestOfTwo(); // find largest of two numbers
 
-    f1.calculateLargestOfTwo(); // calculate factorial of n
+    // // display largest number
+    cout << "\nLargest Number => ";
+    l1.displayLargestNumber();
 
-    cout << "\nLargestOfTwo of " << n << " => ";
+    // Get three numbers to find largest of 2 numbers
+    cout << "\n\nEnter Three Numbers to Find Largest => ";
+    cin >> n1 >> n2 >> n3;
 
-    f1.displayLargestOfTwo(); // display factorial of n
+    l1.setNumbers(n1, n2, n3); // set numbers to find largest among three numbers
+    l1.findLargestOfThree();   // find largest of two numbers
+
+    // // display largest number
+    cout << "\nLargest Number => ";
+    l1.displayLargestNumber();
 
     cout << endl; // Add new line
     getch();
