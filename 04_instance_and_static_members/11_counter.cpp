42982b6cbc48b5241b0ef0dba776930274da6616
aman <webwithaman06@gmail.com>
2024-01-27 19:47:36 +0530

    add : cpp-programming-questions instance and static mem 11

---
+++ 04_instance_and_static_members/11_counter.cpp
@@ -0,0 +1,38 @@
+// // Implement a class Counter with a static member variable count and a static member function to get value of count.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Counter
+class Counter
+{
+private:
+    // // static member variable
+    static int count;
+
+public:
+    // // static function to get count
+    static int getCount()
+    {
+        return count;
+    }
+};
+
+// // define and initialize static member variables
+int Counter::count = 0;
+
+// // Main Function Start
+int main()
+{
+
+    cout << "\nValue of Count => " << Counter::getCount() << endl;
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
