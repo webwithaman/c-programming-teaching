49ed5333c809695f7098ab5a11e802b1aa4b87de
aman <webwithaman06@gmail.com>
2024-01-30 20:30:06 +0530

    add: 06_operator_friend fun 01

--- 06_operator_overloading_and_friend_function/01_overload_arithmetic.cpp
+++ 06_operator_overloading_and_friend_function/01_overload_arithmetic.cpp
@@ -56,24 +56,106 @@ public:
         cout << "\n"
              << real << " + " << imag << "i" << endl;
     }
+
+    // // overload binary (addition +) operator
+    Complex operator+(Complex c)
+    {
+        c.real = real + c.real;
+        c.imag = imag + c.imag;
+        return c;
+    }
+
+    // // overload binary (subtraction -) operator
+    Complex operator-(Complex c)
+    {
+        c.real = real - c.real;
+        c.imag = imag - c.imag;
+        return c;
+    }
+
+    // // overload binary (multiplication *) operator
+    Complex operator*(Complex c)
+    {
+        c.real = real * c.real;
+        c.imag = imag * c.imag;
+        return c;
+    }
+
+    // // overload  relational equality (==) operator
+    bool operator==(Complex c)
+    {
+        if (real == c.real && imag == c.imag)
+            return true;
+
+        return false;
+    }
 };
 
 // // Main Function Start
 int main()
 {
+    Complex c1, c2, c3; // create objects of Complex
     double real, imag;
 
-    // // Get complex number
+    // // Get complex numbers  to add
+    cout << "\n>>>>>>>> Enter Two Complex Numbers to Perform Various Operations <<<<<<<<<\n";
+
+    // // Get first complex number
+    cout << "\n>>>>>>>> Enter First Complex Number <<<<<<<<<\n";
     cout << "\nEnter Real Part => ";
     cin >> real;
     cout << "\nEnter Imaginary Part => ";
     cin >> imag;
 
-    Complex c1(real, imag); // create objects of Complex
+    c1.setData(real, imag); // set complex number for c1
 
-    // // display complex number
-    cout << "\n>>>>>>>> Complex Number <<<<<<<<<\n";
+    // // Get second complex number
+    cout << "\n\n>>>>>>>> Enter Second Complex Number <<<<<<<<<\n";
+    cout << "\nEnter Real Part => ";
+    cin >> real;
+    cout << "\nEnter Imaginary Part => ";
+    cin >> imag;
+
+    c2.setData(real, imag); // set complex number for c2
+
+    // // display first complex number
+    cout << "\n>>>>>>>> First Complex Number <<<<<<<<<\n";
     c1.showData();
+
+    // // display second complex number
+    cout << "\n>>>>>>>> Second Complex Number <<<<<<<<<\n";
+    c2.showData();
+
+    // // add complex numbers
+    c3 = c1 + c2;
+
+    // // display addition of complex numbers
+    cout << "\n>>>>>>>> Addition of Entered Complex Numbers <<<<<<<<<\n";
+    c3.showData();
+
+    // // subtraction complex numbers
+    c3 = c1 - c2;
+
+    // // display subtraction of complex numbers
+    cout << "\n>>>>>>>> Subtraction of Entered Complex Numbers <<<<<<<<<\n";
+    c3.showData();
+
+    // // multiply complex numbers
+    c3 = c1 * c2;
+
+    // // display multiplication of complex numbers
+    cout << "\n>>>>>>>> Multiplication of Entered Complex Numbers <<<<<<<<<\n";
+    c3.showData();
+
+    // // check equality of complex numbers
+    bool isEqual = c1 == c2;
+
+    // // display multiplication of complex numbers
+    cout << "\n>>>>>>>> Entered Complex Numbers Are Equal or Not <<<<<<<<<\n";
+    if (isEqual)
+        cout << "\nEntered Complex Numbers Are Equal";
+    else
+        cout << "\nEntered Complex Numbers Are Not Equal";
 
     cout << endl; // Add new line
     getch();
