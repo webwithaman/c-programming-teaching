a5291376e07fd5ed3ca6a33a6e14ddbc322eddbe
aman <webwithaman06@gmail.com>
2024-01-31 16:37:45 +0530

    add: 07_this_pointer 01

---
+++ 07_this_pointer/01_this_pointer_complex.cpp
@@ -0,0 +1,2 @@
+// // Define a class Complex with appropriate instance variables and member functions.One of the functions should be setData() to set the properties of the object. Make sure the names of formal arguments are the same as names of instance variables.
+
