81539ccd0303c0b4a15eb7af8542d29d7aea3954
aman <webwithaman06@gmail.com>
2024-02-14 13:38:27 +0530

    add : cpp-programming-questions 08_type_casting 00

--- 08_type_casting_and_conversion/01_primitive_to_complex.cpp
+++ 08_type_casting_and_conversion/01_primitive_to_complex.cpp
@@ -11,3 +11,75 @@ Write a C++ program to convert Primitive type to Complex type.
    }
 
 */
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Complex
+class Complex
+{
+
+private:
+    // // instance member variables
+    double a;
+    double b;
+
+public:
+    // // constructors
+    Complex()
+    {
+        a = b = 0;
+    }
+
+    Complex(double r)
+    {
+        a = b = r;
+    }
+
+    Complex(double r, double i)
+    {
+        a = r;
+        b = i;
+    }
+
+    // // instance member function to set compelx number
+    void setData(double r, double i)
+    {
+        a = r;
+        b = i;
+    }
+
+    // // instance member function to display compelx number
+    void showData()
+    {
+        cout << "\n"
+             << a << " + " << b << "i" << endl;
+    }
+};
+
+// // Main Function Start
+int main()
+{
+    double real, imag;
+
+    // // Get complex number
+    cout << "\nEnter Real Part => ";
+    cin >> real;
+    cout << "\nEnter Imaginary Part => ";
+    cin >> imag;
+
+    Complex c1(real, imag); // create objects of Complex
+
+    // // display complex number
+    cout << "\n>>>>>>>> Complex Number <<<<<<<<<\n";
+    c1.showData();
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
