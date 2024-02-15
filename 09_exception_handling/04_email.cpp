cd26073c259a5882b58c89e124a34f4ccb304746
aman <webwithaman06@gmail.com>
2024-02-15 10:10:50 +0530

    add: cpp-prgramming-questions 09_exception_handling 04

---
+++ 09_exception_handling/04_email.cpp
@@ -0,0 +1,47 @@
+// //  Write a C++ program to accept an email address and throw an exception if it does not contain @ symbol.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// // use namespace
+using namespace std;
+
+#define MAX_CHARS 31
+
+// // Main Function Start
+int main()
+{
+
+    try
+    {
+        char email[MAX_CHARS];
+        int found = 0;
+        cout << "\nEnter Your Email Address (MAX_CHAR " << MAX_CHARS - 1 << ") => ";
+        cin.getline(email, MAX_CHARS);
+
+        for (int i = 0; email[i]; i++)
+        {
+            if (email[i] == '@')
+            {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found)
+            throw "Email Address is Invalid, Does not contains '@' symbol";
+
+        cout << "\nEmail is Valid...";
+    }
+    catch (const char *e)
+    {
+        cout << e;
+    }
+
+    cout
+        << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
