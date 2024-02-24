35dc434cdab976c7d23dddb3f127b50939ff769c
aman <webwithaman06@gmail.com>
2024-02-24 21:36:20 +0530

    add: cpp-programming-questions 11_inheritance 02

---
+++ 11_inheritance/02_add.cpp
@@ -0,0 +1,73 @@
+// // Write a C++ program to add two numbers using single inheritance. Accept and add these two numbers in base class and display the sum of these two numbers in derived class.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Base
+class Base
+{
+
+private:
+    // // instance member variables
+    int num1, num2;
+
+protected:
+    // // instance member function to get sum
+    // int getSum() const
+    // {
+    //     return sum;
+    // }
+    int sum;
+
+public:
+    // // instance member function to set numbers
+    void setNumbers(int num1, int num2)
+    {
+        this->num1 = num1;
+        this->num2 = num2;
+    }
+
+    // // instance member function to add numbers
+    void addNumbers()
+    {
+        sum = num1 + num2;
+    }
+};
+
+// // define class Derived
+class Derived : public Base
+{
+
+public:
+    void displaySum()
+    {
+        cout << "\nSum of Numbers => " <<sum;
+    }
+};
+
+// // Main Function Start
+int main()
+{
+    // // create an instance of Base class
+    Base b1;
+
+    // // set numbers
+    b1.setNumbers(5, 15);
+    b1.addNumbers();
+
+    // // create an instance of Derived class
+    Derived d1;
+
+    // // display sum of numbers
+    d1.displaySum();
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
