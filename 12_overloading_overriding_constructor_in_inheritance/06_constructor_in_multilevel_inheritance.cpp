75002cfecbe591df4a4055d0c369ba8f53a10c5b
aman <webwithaman06@gmail.com>
2024-02-29 12:43:11 +0530

    add: cpp-programming-questions 12_overload_overriding_constructor_inheritance 06

--- 12_overloading_overriding_constructor_in_inheritance/06_constructor_in_multilevel_inheritance.cpp
+++ 12_overloading_overriding_constructor_in_inheritance/06_constructor_in_multilevel_inheritance.cpp
@@ -110,17 +110,90 @@ public:
     }
 
     // // instance member function to get roll
-    int setRoll() const
+    int getRoll() const
     {
         return roll;
+    }
+};
+
+// // define class Player by Inheriting class Student
+class Player : public Student
+{
+public:
+    // // static member variable
+    static const int MAX_CHARS_IN_GAME = 31;
+
+private:
+    char *game;
+
+public:
+    // // constructors
+    Player() : Student()
+    {
+        game = new char[1];
+        game[0] = '\0'; // // terminate with null char
+        cout << "\nDefault Constructor of Class Player";
+    }
+
+    Player(const char *name, int roll, const char *game) : Student(name, roll)
+    {
+        int length = strlen(game);
+
+        this->game = new char[length > MAX_CHARS_IN_GAME - 1 ? MAX_CHARS_IN_GAME : length + 1];
+
+        if (length > MAX_CHARS_IN_GAME - 1)
+        {
+            strncpy(this->game, game, MAX_CHARS_IN_GAME - 1);
+            this->game[MAX_CHARS_IN_GAME - 1] = 0; // // terminate with null character
+        }
+        else
+        {
+            strcpy(this->game, game);
+        }
+        cout << "\nParametrized Constructor of Class Player";
+    }
+
+    // // instance member function to set name
+    void setGame(const char *name)
+    {
+        int length = strlen(game);
+
+        this->game = new char[length > MAX_CHARS_IN_GAME - 1 ? MAX_CHARS_IN_GAME : length + 1];
+
+        if (length > MAX_CHARS_IN_GAME - 1)
+        {
+            strncpy(this->game, game, MAX_CHARS_IN_GAME - 1);
+            this->game[MAX_CHARS_IN_GAME - 1] = 0; // // terminate with null character
+        }
+        else
+        {
+            strcpy(this->game, game);
+        }
+    }
+
+    // // instance member function to get game
+    const char *getGame() const
+    {
+        return game;
+    }
+
+    // // destructor
+    ~Player()
+    {
+        delete[] game;
     }
 };
 
 // // Main Function Start
 int main()
 {
-    // // instance of B
-    B b1, b2 = 6;
+    // // instance of Player
+    Player p1("Aman", 7022, "Cricket");
+
+    cout<<"\nName => "<<p1.getName();
+    cout<<"\nRoll => "<<p1.getRoll();
+    cout<<"\nGame => "<<p1.getGame();
+
 
     cout << endl; // Add new line
     getch();
