6b79bc442e82ef028dee6fa0b9291d3cac3955a5
aman <webwithaman06@gmail.com>
2024-02-29 09:08:30 +0530

    add: cpp-programming-questions 12_overload_overriding_constructor_inheritance 02

---
+++ 12_overloading_overriding_constructor_in_inheritance/02_rectangle.cpp
@@ -0,0 +1,96 @@
+// // Define a class Rectangle and overload area function for different types of data type.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Rectangle
+class Rectangle
+{
+
+private:
+    // // instance member variables
+    double length, breadth, area;
+
+public:
+    // // constructors
+    Rectangle()
+    {
+        length = breadth = 0;
+    }
+
+    Rectangle(double l, double b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    // // instance member function to set length of rectangle
+    void setLength(int l)
+    {
+        length = l;
+    }
+
+    // // instance member function to set breadth of rectangle
+    void setBreadth(int b)
+    {
+        breadth = b;
+    }
+
+    // // instance member function to set dimensions of rectangle
+    void setDimensions(int l, int b)
+    {
+        setLength(l);
+        setBreadth(b);
+    }
+
+    // // instance member function to get length of rectangle
+    double getLength(int l)
+    {
+        return length;
+    }
+
+    // // instance member function to get breadth of rectangle
+    double getBreadth(int b)
+    {
+        return breadth;
+    }
+
+    // // instance member function to calculate area of rectangle
+    double calculateArea()
+    {
+        area = length * breadth;
+        return area;
+    }
+
+    // // instance member function to get the area of rectangle
+    double getArea()
+    {
+        return area;
+    }
+};
+
+// // Main Function Start
+int main()
+{
+    double l, b, area;
+
+    // // Get length and breadth of a rectangle to find its area
+    cout << "\nEnter Length and Breadth of A Rectangle => ";
+    cin >> l >> b;
+
+    Rectangle rec1(l, b); // create object of Rectangle
+
+    area = rec1.calculateArea(); // find area
+
+    // // display area of rectangle
+    cout << "\nArea of Rectangle => " << area;
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
