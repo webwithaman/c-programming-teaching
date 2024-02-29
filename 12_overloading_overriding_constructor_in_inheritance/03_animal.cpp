8e134a94736fe669d296ce55a0cd2a02c5ebd4f1
aman <webwithaman06@gmail.com>
2024-02-29 10:25:22 +0530

    add: cpp-programming-questions 12_overload_overriding_constructor_inheritance 03

---
+++ 12_overloading_overriding_constructor_in_inheritance/03_animal.cpp
@@ -0,0 +1,46 @@
+// // Define a base class Animal having member function sound(). Define another derived class from Animal class named Dog. You need to override the sound function of the base class in the derived class.
+
+// // Header files
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+// // use namespace
+using namespace std;
+
+// // define class Animal
+class Animal
+{
+public:
+    // // instance member function to produce sound
+    void sound()
+    {
+        cout << "\nSound of Animal...";
+    }
+};
+
+// // define class Dog
+class Dog
+{
+public:
+    // // instance member function to produce sound
+    void sound()
+    {
+        cout << "\nSound of Dog...";
+    }
+};
+
+// // Main Function Start
+int main()
+{
+    Animal a1;
+    a1.sound();
+
+    Dog d1;
+    d1.sound();
+
+    cout << endl; // Add new line
+    getch();
+    return 0;
+}
+// // Main Function End
