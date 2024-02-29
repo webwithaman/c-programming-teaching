7c3e7fabe31c9e24bf6dadb738d610460fee9d0a
aman <webwithaman06@gmail.com>
2024-02-29 21:50:53 +0530

    add: cpp-programming-questions 13_virtual_abstract 03

--- 13_virtual_function_and_abstract_class/02_extend_01.cpp
+++ 13_virtual_function_and_abstract_class/02_extend_01.cpp
@@ -1,4 +1,4 @@
-// // Extend the above program to display the area of circles. This requires the addition of a new derived class 'circle' that computes the area of a circle. Remember, for a circle we need only one value, its radius, but the get_data() function in the base class requires two values to be passed. (Hint: Make the second argument of get_data() function as a default one with zero value.)
+// // Extend the above program to display the area of circles. This requires the addition of a new derived class 'Circle' that computes the area of a circle. Remember, for a circle we need only one value, its radius, but the setData() function in the base class requires two values to be passed. (Hint: Make the second argument of setData() function as a default one with zero value.)
 
 // // Header files
 #include <iostream>
