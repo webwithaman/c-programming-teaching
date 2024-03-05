51a76663d8b332cbe2d3b1601b34ade63b1fc10d
aman <webwithaman06@gmail.com>
2024-03-05 14:12:36 +0530

    add: cpp-programming-questions 14_file_handling 09

--- 14_file_handling/09_employee_data/09_employee_data.cpp
+++ 14_file_handling/09_employee_data/09_employee_data.cpp
@@ -5,7 +5,7 @@ a. Emp Id
 b. Emp Name
 c. Emp Address
 d. Emp Dept (Admin/Sales/Production/IT)
-e. Emp Phone
+e. Emp Phone Number
 f. Emp Age
 
 Write a C++ program to read the above file. Create a new file such as adm.dat,
@@ -36,7 +36,7 @@ public:
 private:
     // // instance member variables
     unsigned int id, age;
-    char name[MAX_CHARS_IN_NAME], address[MAX_CHARS_IN_ADDRESS], department[MAX_CHARS_IN_DEPARTMENT], phone[MAX_CHARS_IN_PHONE];
+    char name[MAX_CHARS_IN_NAME], address[MAX_CHARS_IN_ADDRESS], department[MAX_CHARS_IN_DEPARTMENT], phoneNumber[MAX_CHARS_IN_PHONE];
 
 public:
     // // constructors
@@ -47,15 +47,15 @@ public:
         name[0] = 0;
         address[0] = 0;
         department[0] = 0;
-        phone[0] = 0;
+        phoneNumber[0] = 0;
     }
 
-    Employee(int id, const char *name, int age, const char *phone, const char *address, const char *department)
+    Employee(int id, const char *name, int age, const char *phoneNumber, const char *address, const char *department)
     {
         this->id = id;
         this->age = age;
         strcpy(this->name, name);
-        strcpy(this->phone, phone);
+        strcpy(this->phoneNumber, phoneNumber);
         strcpy(this->address, address);
         strcpy(this->department, department);
     }
@@ -102,16 +102,16 @@ public:
         return name;
     }
 
-    // // instance member function to set phone
-    void setPhone(const char *phone)
+    // // instance member function to set phoneNumber
+    void setPhoneNumber(const char *phoneNumber)
     {
-        strcpy(this->phone, phone);
+        strcpy(this->phoneNumber, phoneNumber);
     }
 
-    // // instance member function to get phone
-    const char *getPhone()
+    // // instance member function to get phoneNumber
+    const char *getPhoneNumber()
     {
-        return phone;
+        return phoneNumber;
     }
 
     // // instance member function to set address
@@ -141,45 +141,85 @@ public:
     // // instance member function to input and set employee data
     void inputEmployeeData()
     {
-        int empId;
-        char bookTitle[Employee::MAX_CHARS_IN_TITLE];
-        double bookPrice;
+        int id;
 
-        // // Get book id
+        // // get and set employee id
         cout << "\nEnter Employee Id => ";
-        cin >> empId;
+        cin >> id;
 
-        // // Get book title
-        cout << "\nEnter Employee Title (MAX_CHARS " << Employee::MAX_CHARS_IN_TITLE - 1 << ") => ";
+        // // set id
+        setId(id);
+
+        // // get name
+        cout << "\nEnter Employee Name (MAX_CHARS " << Employee::MAX_CHARS_IN_NAME - 1 << ") => ";
         cin.ignore();
-        cin.getline(bookTitle, Employee::MAX_CHARS_IN_TITLE);
+        cin.getline(name, Employee::MAX_CHARS_IN_NAME);
 
-        // // Get book price
-        cout << "\nEnter Employee Price => ";
-        cin >> bookPrice;
+        // // get age
+        cout << "\nEnter Employee Age => ";
+        cin >> age;
 
-        // // set data
-        setEmployeeId(empId);
-        setEmployeeTitle(bookTitle);
-        setEmployeePrice(bookPrice);
+        // // get phone number
+        cout << "\nEnter Employee Phone Number (MAX_CHARS " << Employee::MAX_CHARS_IN_PHONE - 1 << ") => ";
+        cin.ignore();
+        cin.getline(phoneNumber, Employee::MAX_CHARS_IN_PHONE);
+
+        // // get address
+        cout << "\nEnter Employee Address (MAX_CHARS " << Employee::MAX_CHARS_IN_ADDRESS - 1 << ") => ";
+        cin.ignore();
+        cin.getline(address, Employee::MAX_CHARS_IN_ADDRESS);
+
+        // // get department
+        int choice;
+        while (true)
+        {
+            cout << "\n>>>>>>>>>> Choose Department <<<<<<<<<<<";
+            cout << "\nPress 1. Admin";
+            cout << "\nPress 2. Sales";
+            cout << "\nPress 3. Production";
+            cout << "\nPress 4. IT";
+            cout << "\nEnter Department Number => ";
+            cin >> choice;
+
+            if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
+            {
+                cout << "\n!!!! Invalid Department Number... Try Again" << endl;
+                cin.ignore();
+            }
+            else
+            {
+                if (choice == 1)
+                    strcpy(department, "Admin");
+                else if (choice == 2)
+                    strcpy(department, "Sales");
+                else if (choice == 3)
+                    strcpy(department, "Production");
+                else
+                    strcpy(department, "IT");
+                break;
+            }
+        }
     }
 
-    // // instance member function to show book data
-    void showEmployeeData()
+    // // instance member function to show employee data
+    void showEmployeeData() const
     {
-        cout << "\nEmployee Id => " << empId;
-        cout << "\nEmployee Title => " << bookTitle;
-        cout << "\nEmployee Price => " << bookPrice;
+        cout << "\nEmployee Id => " << id;
+        cout << "\nEmployee Name => " << name;
+        cout << "\nEmployee Age => " << age;
+        cout << "\nEmployee Phone Number => " << phoneNumber;
+        cout << "\nEmployee Address => " << address;
+        cout << "\nEmployee Department => " << department;
     }
 
-    // // instance member function to store book record
+    // // instance member function to store employee data
     int storeEmployeeData()
     {
-        if (empId == -1 || bookPrice == -1)
-            return 0; // book data not stored
+        if (id == -1 || age == -1)
+            return 0; // employee data not stored
 
         // // specify file name
-        const char *fileName = "books_data.dat";
+        const char *fileName = "emp.dat";
 
         // create an instance of ofstream for writing in a file
         ofstream fout;
@@ -189,19 +229,19 @@ public:
 
         // // check if the file is successfully opened
         if (!fout.is_open())
-            return 0; // book data not stored
+            return 0; // employee data not stored
 
         fout.write((char *)this, sizeof(*this));
 
-        return 1; // book data successfully stored
+        return 1; // employee data successfully stored
     }
 };
 
-// // function to fetch books data from a fike and show
+// // function to fetch employees data from a fike and show
 void fetchAndShowEmployeeData()
 {
     // // specify file name
-    const char *fileName = "books_data.dat";
+    const char *fileName = "emp.dat";
 
     // create an instance of ifstream for reading from a file
     ifstream fin;
@@ -230,11 +270,11 @@ void fetchAndShowEmployeeData()
     }
 }
 
-// // function to search a book from file using book id
+// // function to search a employee from file using employee id
 void searchEmployeeById(int empId)
 {
     // // specify file name
-    const char *fileName = "books_data.dat";
+    const char *fileName = "employees_data.dat";
 
     // create an instance of ifstream for reading from a file
     ifstream fin;
@@ -257,7 +297,7 @@ void searchEmployeeById(int empId)
 
     while (!fin.eof())
     {
-        if (tempEmployee.getEmployeeId() == empId)
+        if (tempEmployee.getId() == empId)
         {
             cout << "\n>>>>>>>>> Employee Found <<<<<<<<" << endl;
             tempEmployee.showEmployeeData();
@@ -288,39 +328,39 @@ int main()
     }
 
     // // dynamically allocate memory for n objects of Employee
-    Employee *books = new Employee[n];
+    Employee *employees = new Employee[n];
 
-    // // input, set and store books data
+    // // input, set and store employees data
     cout << "\n>>>>>>>>>> Enter Data of " << n << " Employees <<<<<<<<<<<<<" << endl;
     for (int i = 0; i < n; i++)
     {
         cout << "\n>>>>>>>>>>> Enter Data of Employee " << i + 1 << " <<<<<<<<<<<<" << endl;
 
-        // // input and set book data
-        books[i].inputEmployeeData();
+        // // input and set employee data
+        employees[i].inputEmployeeData();
 
-        // // store book data
-        if (!(books[i].storeEmployeeData()))
+        // // store employee data
+        if (!(employees[i].storeEmployeeData()))
         {
             cout << "\n!!! Employee Data Not Stored..." << endl;
             return 0;
         }
     }
 
-    // // books data stored successfully
+    // // employees data stored successfully
     cout << "\nEmployees Data Successfully Stored..." << endl;
 
-    // // read and show books data
+    // // read and show employees data
     cout << "\n>>>>>>>>>> Employees Data Stored In File <<<<<<<<<<<<<<";
     fetchAndShowEmployeeData();
 
-    // // get book id to search a book in file
+    // // get employee id to search a employee in file
     int empId;
 
     cout << "\nEnter Employee Id to Search A Employee => ";
     cin >> empId;
 
-    // // search book
+    // // search employee
     searchEmployeeById(empId);
 
     cout << endl; // Add new line
